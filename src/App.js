40008430e9d4c5167f568bbc7b0a0ae39b9b7cb3
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-31 14:45:08 +0700

    add return

--- src/App.js
+++ src/App.js
@@ -4,4 +4,28 @@ export default function FeedbackForm() {
   const [text, setText] = useState('');
   const [isSending, setIsSending] = useState(false);
   const [isSent, setIsSent] = useState(false);
+
+  async function handleSumit(e) {
+    e.preventDefault();
+    
+  }
+
+  return (
+    <form onSubmit={handleSumit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+      disabled={isSending}
+      value={text}
+      onChange={e => setText(e.target.value)}
+      />
+      <br/>
+      <button
+      disabled={isSending}
+      type="submit"
+      >
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  )
 }
